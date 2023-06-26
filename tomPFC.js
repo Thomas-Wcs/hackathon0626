cba89e65e9cef75a350d356509c801b3d0ced90b
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-06-26 17:15:01 +0200

    optimisationde PFC

---
+++ tomPFC.js
@@ -0,0 +1,42 @@
+let joueurScore = 0;
+let robotScore = 0;
+let egaliteScore = 0;
+
+// const playerName = prompt('Veuillez entrer votre nom :');
+const buttons = document.querySelectorAll('button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    const joueur = buttons[i].innerHTML.trim();
+    const robot =
+      buttons[Math.floor(Math.random() * buttons.length)].innerHTML.trim();
+    console.log(` "${joueur}","${robot}" `);
+
+    let resultat = '';
+
+    if (
+      (joueur === 'Pierre' && robot === 'Ciseaux') ||
+      (joueur === 'Feuille' && robot === 'Pierre') ||
+      (joueur === 'Ciseaux' && robot === 'Feuille')
+    ) {
+      resultat = 'Le joueur a gagné';
+      joueurScore++;
+    } else if (joueur === robot) {
+      resultat = 'Égalité';
+      egaliteScore++;
+    } else {
+      resultat = 'Le joueur a perdu';
+      robotScore++;
+    }
+
+    const scoreMessage = `Scores : Joueur ${joueurScore} | Robot ${robotScore} | Égalité ${egaliteScore}`;
+    const resultMessage = `Joueur : ${joueur}. Robot : ${robot}. Résultat : ${resultat} !`;
+    const humanScore = ` Joueur : ${joueurScore}  `;
+    const robotDisplayScore = ` Robot : ${robotScore}  `;
+
+    document.querySelector('.joueur-annonce').innerHTML = humanScore;
+    document.querySelector('.robot-annonce').innerHTML = robotDisplayScore;
+
+    document.querySelector('.result-annonce').innerHTML = resultMessage;
+  });
+}
